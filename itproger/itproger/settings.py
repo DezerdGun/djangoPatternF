72c24882871d6d57a087176eaf8cfc0a23274f47
DezerdGun <mir6raid@gmail.com>
2024-08-23 17:55:15 +0500

    connection with mysql

--- itproger/itproger/settings.py
+++ itproger/itproger/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'itproger.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'test',  # Replace with your database name
+        'USER': 'root',  # Replace with your MySQL username
+        'PASSWORD': 'Str0ngP@ssw0rd!',  # Replace with your MySQL password
+        'HOST': 'localhost',  # Set to your MySQL server address
+        'PORT': '3306',  # Default MySQL port
     }
 }
 
